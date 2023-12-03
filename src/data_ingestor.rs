c41c88be44c6a1950c198c292245213c9e113317
Pranav Manivannan <pranavmani04@gmail.com>
2023-12-03 14:28:13 -0500

    Cleaned up data_ingestor for prod

--- src/data_ingestor.rs
+++ src/data_ingestor.rs
@@ -6,8 +6,7 @@ use std::env;
 use std::sync::mpsc;
 use std::time::SystemTime;
 
-const ORGANIZATION: &str = "devteam"; // replace with organization name
-const ORG_ID: &str = ""; // replace w org id
+const ORGANIZATION: &str = "devteam";
 
 /// A struct for setting a channel receiver endpoint and uploading the messages to data storage services.
 pub struct DataIngestor {
@@ -20,8 +19,8 @@ pub struct DataIngestor {
 
 /// A struct for making a buffer
 pub struct Buffer {
-    pub storage: Vec<String>, // buffer
-    pub bucket: String,       // bucketname
+    pub storage: Vec<String>,
+    pub bucket: String,
 }
 
 /// An implementation of DataIngestor w constructor and receiving/uploading methods
@@ -96,33 +95,11 @@ impl DataIngestor {
 
         buffer.storage.push(message);
 
-        //push to influx if capacity is reached
         if buffer.storage.len() > self.buffer_capacity {
             match buffer.push_data(&self.client).await {
-                Ok(response_body) => {
+                Ok(()) => {
                     println!("Successful Push");
                     buffer.storage.clear();
-
-                    //time for testing purpose
-                    let input: DateTime<Utc> = Utc::now();
-                    input.to_rfc3339_opts(chrono::SecondsFormat::Micros, true);
-                    let format = "%Y-%m-%d %H:%M:%S%.f %Z";
-                    let mut output: String = "".to_owned();
-                    match Utc.datetime_from_str(&input.to_string(), format) {
-                        Ok(datetime) => {
-                            output = datetime.to_rfc3339_opts(chrono::SecondsFormat::Micros, true);
-                            println!("Formatted date-time: {}", output);
-                        }
-                        Err(e) => println!("Error parsing date-time: {}", e),
-                    }
-
-                    let _ = Buffer::query_data(
-                        &self.client,
-                        "2023-01-01T00:00:00Z".to_owned(),
-                        output,
-                        "Binance".to_owned(),
-                    )
-                    .await;
                 }
 
                 Err(err) => {
@@ -150,7 +127,6 @@ impl Buffer {
         bucket_name: String,
     ) -> Result<(), Box<dyn std::error::Error>> {
         dotenv().ok();
-        let organization = ORGANIZATION;
         let bucket_name = &bucket_name;
 
         let flux_query = "from(bucket: \"".to_owned()
@@ -164,7 +140,7 @@ impl Buffer {
 
         let url = format!(
             "https://us-east-1-1.aws.cloud2.influxdata.com/api/v2/query?org={}",
-            organization
+            ORGANIZATION
         );
 
         let response = client
@@ -202,14 +178,13 @@ impl Buffer {
     /// Pushes the data in a buffer to an InfluxDB bucket.
     pub async fn push_data(&self, client: &Client) -> Result<(), Box<dyn std::error::Error>> {
         dotenv().ok();
-        let organization = ORGANIZATION;
         let data = self.storage.join("\n");
         let bucket_name = &self.bucket;
         print!("{}", data);
 
         let url = format!(
             "https://us-east-1-1.aws.cloud2.influxdata.com/api/v2/write?org={}&bucket={}",
-            organization, bucket_name,
+            ORGANIZATION, bucket_name,
         );
 
         let api_token = env::var("API_TOKEN").expect("API_TOKEN must be set");
